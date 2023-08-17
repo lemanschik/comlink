2b4397074860677c4e16b91508e7d85b1b2a06e9
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-08-17 12:41:12 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,43 +2,35 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import { sync } from "rimraf";
 
-const createRollupConfig = ({ moduleName, ext = "js" }) => ({
-  input: `./src/${input}.ts`,
-  output: {
-    name: "Comlink",
-    file: `dist/${moduleName}/${moduleName}.${ext}`,
-    format: ext.endsWith("mjs") ? 'esm' : 'umd',
-    sourcemap: true,
-  },
-  plugins: [
-    typescript({
-      tsconfig: "./tsconfig.json",
-      compilerOptions: {
-        declaration: true,
-        declarationDir: ".",
-        sourceMap: true,
-        outDir: "dist",
-      },
-    }),
-    ext.startsWith("min") && terser({
-        compress: true,
-        mangle: true,
-    })
-  ].filter(Boolean),
-});
-
-
 sync("dist");
 
 export default [
-  { input: "comlink", ext: "mjs" },
-  { input: "comlink", ext: "min.mjs" },
-  { input: "comlink", ext: "js" },
-  { input: "comlink", ext: "min.js" },
-  { input: "comlink", ext: "js" },
-  { input: "comlink", ext: "min.js" },
-  { input: "node-adapter", ext: "mjs" },
-  { input: "node-adapter", ext: "min.mjs" },
-  { input: "node-adapter", ext: "js" },
-  { input: "node-adapter", ext: "min.js" },
-].map(createRollupConfig);
+  "mjs","min.mjs","js","min.js"
+].map((ext) => ["comlink","node-adapter"].map(
+  (moduleName) => [moduleName, ext])
+)).flatMap(
+  ([moduleName, ext]) => ({
+    input: `./src/${input}.ts`,
+    output: {
+      name: "Comlink",
+      file: `dist/${moduleName}/${moduleName}.${ext}`,
+      format: ext.endsWith("mjs") ? 'esm' : 'umd',
+      sourcemap: true,
+    },
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.json",
+        compilerOptions: {
+          declaration: true,
+          declarationDir: ".",
+          sourceMap: true,
+          outDir: "dist",
+        },
+      }),
+      ext.startsWith("min") && terser({
+          compress: true,
+          mangle: true,
+      })
+    ].filter(Boolean),
+  })
+);
