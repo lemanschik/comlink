08b7e0106830d089dab2110acd7a92e854a5b58d
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-08-17 12:22:06 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,48 +2,43 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import { sync } from "rimraf";
 
-function config({ format, minify, input, ext = "js" }) {
-  const dir = `dist/${format}/`;
-  const minifierSuffix = minify ? ".min" : "";
-  return {
-    input: `./src/${input}.ts`,
-    output: {
-      name: "Comlink",
-      file: `${dir}/${input}${minifierSuffix}.${ext}`,
-      format,
-      sourcemap: true,
-    },
-    plugins: [
-      typescript({
-        tsconfig: "./tsconfig.json",
-        compilerOptions: {
-          declaration: true,
-          declarationDir: ".",
-          sourceMap: true,
-          outDir: "dist",
-        },
-      }),
-      minify
-        ? terser({
-            compress: true,
-            mangle: true,
-          })
-        : undefined,
-    ].filter(Boolean),
-  };
-}
+const createRollupConfig = ({ moduleName, ext = "js" }) => ({
+  input: `./src/${input}.ts`,
+  output: {
+    name: "Comlink",
+    file: `dist/${moduleName}/${moduleName}.${ext}`,
+    format: ext.endsWith("mjs") ? 'esm' : 'umd',
+    sourcemap: true,
+  },
+  plugins: [
+    typescript({
+      tsconfig: "./tsconfig.json",
+      compilerOptions: {
+        declaration: true,
+        declarationDir: ".",
+        sourceMap: true,
+        outDir: "dist",
+      },
+    }),
+    ext.startsWith("min") && terser({
+        compress: true,
+        mangle: true,
+    })
+  ].filter(Boolean),
+});
+
 
 sync("dist");
 
 export default [
-  { input: "comlink", format: "esm", minify: false, ext: "mjs" },
-  { input: "comlink", format: "esm", minify: true, ext: "mjs" },
-  { input: "comlink", format: "esm", minify: false },
-  { input: "comlink", format: "esm", minify: true },
-  { input: "comlink", format: "umd", minify: false },
-  { input: "comlink", format: "umd", minify: true },
-  { input: "node-adapter", format: "esm", minify: false, ext: "mjs" },
-  { input: "node-adapter", format: "esm", minify: true, ext: "mjs" },
-  { input: "node-adapter", format: "umd", minify: false },
-  { input: "node-adapter", format: "umd", minify: true },
-].map(config);
+  { input: "comlink", ext: "mjs" },
+  { input: "comlink", ext: "min.mjs" },
+  { input: "comlink", ext: "js" },
+  { input: "comlink", ext: "min.js" },
+  { input: "comlink", ext: "js" },
+  { input: "comlink", ext: "min.js" },
+  { input: "node-adapter", ext: "mjs" },
+  { input: "node-adapter", ext: "min.mjs" },
+  { input: "node-adapter", ext: "js" },
+  { input: "node-adapter", ext: "min.js" },
+].map(createRollupConfig);
